14ff0e675c3bd1f80e87cfccb82606d757a2ee7d
tannerp647 <tannerp647@gmail.com>
2023-02-04 11:37:41 -0700

    set up photo endpoint and sql query and get photos

--- server.py
+++ server.py
@@ -1,8 +1,17 @@
 import boto3
 import psycopg2
 
+from typing import List
+from pydantic import BaseModel
+
 import uvicorn
 from fastapi import FastAPI, UploadFile
+
+class PhotoModel(BaseModel):
+        id: int
+        photo_name: str
+        photo_url: str
+        is_deleted: bool
 
 app = FastAPI(debug=True)
 
@@ -10,8 +19,32 @@ app = FastAPI(debug=True)
 async def check_status():
         return "Hello World"
 
+@app.get("/photos", response_model=List[PhotoModel])
+async def get_all_photos():
+        #connect to database
+        conn = psycopg2.connect(
+                database="exampledb", user="docker", password="docker", host="0.0.0.0"
+        )
+        cur = conn.cursor()
+        cur.execute("select * from photo order by id desc")
+        rows = cur.fetchall()
+
+        formatted_photos = []
+        for row in rows:
+                formatted_photos.append(
+                        PhotoModel(
+                                id=row[0],
+                                photo_name=row[1],
+                                photo_url=row[2],
+                                is_deleted=row[3]
+                        )
+                )
+        cur.close()
+        conn.close()
+        return formatted_photos
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, reload=False)
-
 
 
