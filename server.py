3cdc3a62a422f12b25dd3f912e908e09404c1ca1
tannerp647 <tannerp647@gmail.com>
2023-02-04 11:20:15 -0700

    fastApi setup

---
+++ server.py
@@ -0,0 +1,17 @@
+import boto3
+import psycopg2
+
+import uvicorn
+from fastapi import FastAPI, UploadFile
+
+app = FastAPI(debug=True)
+
+@app.get("/status")
+async def check_status():
+        return "Hello World"
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=False)
+
+
+
